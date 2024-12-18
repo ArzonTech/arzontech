d32d9696ad40109f67464319a1c26e6240ccbc9f
ArzonTech <comandotimarketing@gmail.com>
2024-12-18 11:12:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const observerOptions = {
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('is-visible');
+            }
+        });
+    }, observerOptions);
+
+    document.querySelectorAll('.fade-in').forEach((element) => {
+        observer.observe(element);
+    });
+});
+
+
